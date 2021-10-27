859845eb709e9147b428325abc2764713613f208
Tushar Khete <tusharkhete118@gmail.com>
2021-10-27 23:45:15 +0530

    Basic-flutter-app

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bmi/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI(Body mass index) calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
